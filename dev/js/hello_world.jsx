06014fc97f2ea9f4058b631673d1cc61f70e1f6c
jie.liang <jie.liang@intel.com>
2016-12-29 17:15:47 +0800

    add pre-commit

--- dev/js/hello_world.jsx
+++ dev/js/hello_world.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 
+/* eslint react/prop-types: 0  react/prefer-stateless-function: 0*/
 class HelloWorld extends React.Component {
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,49 @@
 const path = require('path');
 const webpack = require('webpack');
+
 module.exports = {
   entry: {
-    main: "./dev/js/index.jsx",
-    app: "./dev/js/app.jsx"
+    main: './dev/js/index.jsx',
+    app: './dev/js/app.jsx',
   },
   output: {
-    path: path.join(__dirname, "/out/js"),
-    filename: "[name].bundle.js",
-    chunkFilename: "[name].chunk.js",
-    publicPath: "/out/js/"
+    path: path.join(__dirname, '/out/js'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
+    publicPath: '/out/js/',
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
   performance: {
-    hints: false
+    hints: false,
   },
   devServer: {
     inline: true,
     colors: true,
     progress: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   module: {
     loaders: [
       {
         test: /\.js|jsx$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader']
-      }
-    ]
+        loaders: ['babel-loader'],
+      },
+    ],
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
+        warnings: false,
       },
       output: {
-        comments: false
-      }
+        comments: false,
+      },
     }),
     new webpack.optimize.CommonsChunkPlugin({
-      names: ["common", "vendor"]
-    })
-  ]
-}
+      names: ['common', 'vendor'],
+    }),
+  ],
+};
