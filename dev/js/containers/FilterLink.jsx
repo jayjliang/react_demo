4e501d99aac45fca22eebc612fea8ae1dfcff9cc
LiangJ <jerry.liangj@qq.com>
2017-02-18 20:44:54 +0800

    test remember git passwd

--- dev/js/containers/FilterLink.jsx
+++ dev/js/containers/FilterLink.jsx
@@ -3,14 +3,14 @@ import { setVisibilityFilter } from '../actions'
 import Link from '../components/Link'
 
 const mapStateToProps = (state, ownProps) => {
-  console.log("state", ownProps);
+  // console.log("state", ownProps);
   return {
     active: ownProps.filter === state.visibilityFilter
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  console.log("dispatch", ownProps);
+  // console.log("dispatch", ownProps);
   return {
     onClick: () => {
       dispatch(setVisibilityFilter(ownProps.filter))
